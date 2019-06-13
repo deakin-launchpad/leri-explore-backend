428a9704dfc7b3b4f0b5d3f577b7ec6d41fe50e1
sanchitd5 <sanchitd5@live.com>
2019-06-13 16:55:11 +1000

    Login and Register routes for researcher

---
+++ routes/researcherRoutes.js
@@ -0,0 +1,81 @@
+const UniversalFunctions = require("../utils/UniversalFunctions")
+const Joi = require("joi")
+const Config = require("../config")
+const Controllers = require("../controllers")
+
+const researcherLogin = {
+  method: "POST",
+  path: "/api/researcher/login",
+  config: {
+    description: "Researcher Login API",
+    tags: ["api", "researcher"],
+    handler: (request, reply) => {
+      return new Promise((resolve, reject) => {
+        if (!UniversalFunctions.verifyEmailFormat(request.payload.emailId))
+          return reject(UniversalFunctions.sendError(Config.APP_CONSTANTS.STATUS_MSG.ERROR.INVALID_EMAIL_FORMAT));
+        else
+          Controllers.ResearcherController.researcherLogin(request.payload, function (err, data) {
+            if (err) return reject(UniversalFunctions.sendError(err))
+            resolve(
+              UniversalFunctions.sendSuccess(Config.APP_CONSTANTS.STATUS_MSG.SUCCESS.DEFAULT, data)
+            )
+          })
+      })
+    },
+    validate: {
+      payload: {
+        emailId: Joi.string().required(),
+        password: Joi.string().required()
+      },
+      failAction: UniversalFunctions.failActionFunction
+    },
+    plugins: {
+      "hapi-swagger": {
+        responseMessages:
+          UniversalFunctions.CONFIG.APP_CONSTANTS.swaggerDefaultResponseMessages
+      }
+    }
+  }
+}
+
+const researcherRegister = {
+  method: "POST",
+  path: "/api/researcher/register",
+  config: {
+    description: "Researcher Register API",
+    tags: ["api", "researcher"],
+    handler: (request, h) => {
+      return new Promise((resolve, reject) => {
+        if (!UniversalFunctions.verifyEmailFormat(request.payload.emailId))
+          return reject(UniversalFunctions.sendError(Config.APP_CONSTANTS.STATUS_MSG.ERROR.INVALID_EMAIL_FORMAT));
+        else
+          Controllers.ResearcherController.researcherRegister(request.payload, function (err, data) {
+            if (err) return reject(UniversalFunctions.sendError(err))
+            resolve(
+              UniversalFunctions.sendSuccess(Config.APP_CONSTANTS.STATUS_MSG.SUCCESS.DEFAULT, data)
+            )
+          })
+      })
+    },
+    validate: {
+      payload: {
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        emailId: Joi.string().required(),
+        password: Joi.string().required()
+      },
+      failAction: UniversalFunctions.failActionFunction
+    },
+    plugins: {
+      "hapi-swagger": {
+        responseMessages:
+          UniversalFunctions.CONFIG.APP_CONSTANTS.swaggerDefaultResponseMessages
+      }
+    }
+  }
+}
+
+module.exports = [
+  researcherLogin,
+  researcherRegister
+]
