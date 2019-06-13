554be52842481ddca889f4b8004a4a16554dc612
Akash Agarwal <akashaggarwal777@gmail.com>
2019-06-13 17:40:00 +1000

    Remove ;'s

--- models/mongo/ResearcherDetailModel.js
+++ models/mongo/ResearcherDetailModel.js
@@ -2,8 +2,8 @@
  * Created by Sanchit on 13/06/19.
  */
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const Researcher = new Schema({
   first_name: { type: String, trim: true, required: true },
@@ -13,6 +13,6 @@ const Researcher = new Schema({
   accessToken: { type: String, index: true, unique: true, sparse: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-});
+})
 
 module.exports = mongoose.model('researcher', Researcher)
