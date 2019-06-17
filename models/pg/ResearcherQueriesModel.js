64dfff0674c8e697fc1d8944b5c470ec0c348559
Akash Agarwal <akashaggarwal777@gmail.com>
2019-06-17 15:01:43 +1000

    ResearcherQueriesModel: Add foreign key constraint to researcherId

--- models/pg/ResearcherQueriesModel.js
+++ models/pg/ResearcherQueriesModel.js
@@ -3,10 +3,20 @@ const sequelizeInstance = require('../../utils/dbHelper').getPGConnection()
 
 class ResearcherQueries extends Sequelize.Model { }
 ResearcherQueries.init({
-  researcherId: Sequelize.INTEGER,
+  researcherId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+
+    references: {
+      // This is a reference to another model
+      model: 'researchers',
+
+      // This is the column name of the referenced model
+      key: 'id',
+    }
+  },
   query: { type: 'JSONB' }
 }, { sequelize: sequelizeInstance, modelName: 'researcher_queries', timestamps: true })
-// TODO: Add unique constraint to all 4 columns together
 
 ResearcherQueries.sync({ force: true }) // TODO: Remove the forcing soon.. This drops the table
 
