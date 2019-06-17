f659d85db7eaa9e29a0f3b7be4c872a32b343f30
Akash Agarwal <akashaggarwal777@gmail.com>
2019-06-17 14:52:40 +1000

    ResearcherModel: Append _id in test environment

--- models/mongo/ResearcherModel.js
+++ models/mongo/ResearcherModel.js
@@ -5,7 +5,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Researcher = new Schema({
+const researcherJSON = {
   first_name: { type: String, trim: true, required: true },
   last_name: { type: String, trim: true, required: true },
   emailId: { type: String, required: true, unique: true },
@@ -13,6 +13,14 @@ const Researcher = new Schema({
   accessToken: { type: String, index: true, unique: true, sparse: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-})
+}
+
+/** 
+ * Appends the user schema JSON with the _id field for test environment.
+ * Helpful since accessToken can be a predefined constant while testing.
+ * */
+if ("test" === process.env.NODE_ENV) researcherJSON._id = { type: Schema.ObjectId, default: function () { return new mongoose.mongo.ObjectId() } }
+
+const Researcher = new Schema(researcherJSON)
 
 module.exports = mongoose.model('researcher', Researcher)
