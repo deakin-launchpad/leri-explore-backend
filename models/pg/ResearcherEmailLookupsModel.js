5dfc0e8443e8c3f0f04362ac4c11d45e8c236da2
Akash Agarwal <akashaggarwal777@gmail.com>
2019-06-24 10:37:06 +1000

    ResearcherEmailLookup: Change emailId to email_id

--- models/pg/ResearcherEmailLookupsModel.js
+++ models/pg/ResearcherEmailLookupsModel.js
@@ -3,7 +3,7 @@ const sequelizeInstance = require('../../utils/dbHelper').getPGConnection()
 
 class ResearcherEmailLookups extends Sequelize.Model { }
 ResearcherEmailLookups.init({
-  emailId: { type: Sequelize.STRING, unique: true, allowNull: false }
+  email_id: { type: Sequelize.STRING, unique: true, allowNull: false }
 }, { sequelize: sequelizeInstance, modelName: 'researcher_email_lookup', timestamps: true })
 
 module.exports = ResearcherEmailLookups
