08ef9ec758a93543405a1b42e9b9bde23424a8a4
Akash Agarwal <akashaggarwal777@gmail.com>
2019-07-30 08:04:50 +1000

    genericLookupsRoutes: Get rid of testRoute

--- routes/genericLookupsRoutes.js
+++ routes/genericLookupsRoutes.js
@@ -37,40 +37,6 @@ const getLookups = {
   }
 }
 
-const testRoute = {
-  method: "POST",
-  path: "/api/testRoute",
-  config: {
-    description: "Query API step 2",
-    auth: 'UserAuth',
-    tags: ["api", "ws", "query"],
-    handler: function (request, h) {
-      return new Promise((resolve, reject) => {
-        
-
-        resolve(
-          HELPER.sendSuccess(Config.APP_CONSTANTS.STATUS_MSG.SUCCESS.DEFAULT)
-        )
-
-      })
-    },
-    validate: {
-      payload: {
-        data: Joi.string()
-      },
-      headers: HELPER.authorizationHeaderObj,
-      failAction: HELPER.failActionFunction
-    },
-    plugins: {
-      "hapi-swagger": {
-        responseMessages:
-          HELPER.CONFIG.APP_CONSTANTS.swaggerDefaultResponseMessages
-      }
-    }
-  }
-}
-
 module.exports = [
-  getLookups,
-  testRoute
+  getLookups
 ]
